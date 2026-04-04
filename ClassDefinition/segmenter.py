1ab603a9d0af6a510d15b953ead48e407f5c18cb
Troy Arthur <trar3243@colorado.edu>
2026-04-04 12:35:25 -0600

    CMDD-1
    Updated for more streamlined function and testing

--- ClassDefinition/segmenter.py
+++ ClassDefinition/segmenter.py
@@ -14,6 +14,8 @@ class Segmenter:
         self.device = device
         self.tokenizer=tokenizer
         self.model = self.__initialize_coref__(self.device)
+        self.method = None # set during run 
+        self.weighted = None # set during run 
 
 
     def __initialize_coref__(self,device):
@@ -50,8 +52,9 @@ class Segmenter:
         return 1.0
     def get_text(self,data_path):
         with open(data_path, 'r') as file:
-            data = json.load(file)
-        return data
+            # data = json.load(file)
+            head = file.readlines() # [next(file) for _ in range(100)]
+        return " ".join(head) # "Reggie is my cat. He is a good boy. He likes to play. Reggie is very loud. He meows all the time. He never ever stops. He is super noisy. He demands his food."
     def __char_to_token__(self,char_idx, offsets):
         for i, (start, end) in enumerate(offsets):
             if start <= char_idx < end:
@@ -60,6 +63,157 @@ class Segmenter:
             if char_idx < start:
                 return max(0, i - 1)
         return len(offsets) - 1
+
+    """def __monolithic_cluster_graph_addition__(self,cluster, text, graph, offsets):
+        # weights is list of token weights 
+        # cluster is list of tuples 
+
+        # use the maximum weight in the entire cluster for this SINGLE edge 
+        weight = max(self.__get_accessibility_score__(text[s:e]) for s, e in cluster)
+
+        c_start = min(span[0] for span in cluster) # starting character index of the entire cluster 
+        c_end = max(span[1] for span in cluster)  # ending character index of the entire cluster 
+        
+        t_start = self.__char_to_token__(c_start, offsets) # starting token index of the entire cluster 
+        t_end = self.__char_to_token__(c_end, offsets) # ending token index of the entire cluster 
+
+        i = t_start 
+        while(i <= t_end):
+            graph[i] = graph[i] + weight 
+            i = i + 1 
+        return graph  """
+
+    # basic idea: draw an edge from each mention to the most recent prior mention. Edge weighted according to the accessability of the postcedent, if self.weighted==True 
+    def __most_recent_cluster_graph_addition__(self,cluster,text,graph,offsets):
+        prior_t_start = None 
+        # prior_t_end = None 
+        for i in range(len(cluster)):
+            if (i==0): 
+                prior_t_start = self.__char_to_token__(cluster[i][0], offsets)
+                # prior_t_end = self.__char_to_token__(cluster[i][1], offsets)
+                continue # not drawing an edge for first mention.. |E| = |V| - 1 
+            t_start = self.__char_to_token__(cluster[i][0], offsets)
+            t_end = self.__char_to_token__(cluster[i][1], offsets)
+            weight = self.__get_accessibility_score__(text[cluster[i][0]:cluster[i][1]]) if self.weighted else 1.0 
+            while(prior_t_start <= t_end):
+                graph[prior_t_start] = graph[prior_t_start] + weight 
+                prior_t_start = prior_t_start + 1 
+        return graph 
+
+    # basic idea: draw an edge from each mention to the prior LOW ACCESSIBLE mention. Edge weighted according to the accessability of the postcedent, if self.weighted==True 
+    def __most_recent_low_accessible_cluster_graph_addition__(self,cluster,text,graph,offsets):
+        # we can create sub-clusters, then treat those as the clusters for the second mode 
+        sub_clusters = list() 
+        sub_cluster = list()
+        for i in range(len(cluster)):
+            mention_score = self.__get_accessibility_score__(text[cluster[i][0]:cluster[i][1]])
+            if(mention_score <= 3.0 and len(sub_cluster)): # if it is a proper noun or something like "the king"
+                sub_clusters.append(sub_cluster)
+                sub_cluster = list()
+            sub_cluster.append(cluster[i])
+        sub_clusters.append(sub_cluster)
+        for sub_cluster in sub_clusters:
+            graph = self.__most_recent_cluster_graph_addition__(sub_cluster,text,graph,offsets)
+        return graph 
+
+
+
+    # returns an array of chunks 
+    def coreference_chunk_text(self, text, method: str, target_size_tokens: int, search_window: int, full_sentence_inclusion: bool, weighted:bool):
+        ### Initialization ### 
+        if(method not in ["most_recent", "most_recent_low_accessible"]):
+            raise Exception(f'Supplied method {method} not in ["most_recent", "most_recent_low_accessible"]')
+        self.method = method # update the class instance for each run for method. 
+        self.weighted = weighted # update the class instance for each run for weighted. 
+
+        if not text.strip(): return list() 
+        encoded = self.tokenizer(text,return_offsets_mapping=True, add_special_tokens=False) 
+
+        # list of integer tuples. Each tuple refers to the start and end character index for each token 
+        offsets = encoded['offset_mapping'] 
+
+        # number of tokens 
+        num_tokens = len(offsets) # number of tokens 
+
+        # preds is a single-element list of CorefResults. Includes text (original text) and clusters. Each cluster here is a list of words within that cluster
+        preds = self.model.predict(texts=[text]) 
+
+        # clusters with as_string=False is a list of list of tuples. Each higher-level list is a list of clusters. Each cluster contains a list of tuples. Each tuple is the start and end character index of each mention.
+        clusters = preds[0].get_clusters(as_strings=False)
+
+        # for post-processing later 
+        sent_char_spans = [m.span() for m in re.finditer(r'[^.!?\n]+[.!?\n]*', text)] if full_sentence_inclusion else list() 
+        # token_to_sent is a dictionary of key=token index (range 0 to len tokens - 1), value = start and end token indices of sentence to which it belongs 
+        token_to_sent = {}
+        for s_start, s_end in sent_char_spans:
+            t_start = self.__char_to_token__(s_start, offsets)
+            t_end = self.__char_to_token__(s_end - 1, offsets) 
+            
+            for i in range(t_start, t_end + 1):
+                token_to_sent[i] = (t_start, t_end)
+
+        ### graph construction ###
+        # graph spans the tokens, encodes cost of cutting at each token 
+        graph = [0.0] * (num_tokens) 
+        for cluster in clusters:
+            if(method == "most_recent"):
+                graph = self.__most_recent_cluster_graph_addition__(cluster,text,graph,offsets)
+            elif(method == "most_recent_low_accessible"):
+                graph = self.__most_recent_low_accessible_cluster_graph_addition__(cluster, text,graph,offsets)
+
+
+        ### Chunk construction ### 
+        chunks = list()
+        chunk_start = 0 
+        while chunk_start < num_tokens:
+            window_start = chunk_start + target_size_tokens - search_window
+            window_end = chunk_start + target_size_tokens + search_window
+            
+            # take rest of text 
+            if window_start >= num_tokens:
+                chunk_end = num_tokens - 1
+            else:
+                # Cap the window_end so we don't search past the end of the document
+                actual_window_end = min(window_end, num_tokens)
+
+                window_costs = graph[window_start:actual_window_end]
+                # if all identical...
+                if len(set(window_costs)) <= 1:
+                    # Default to the exact target size. 
+                    chunk_end = min(chunk_start + target_size_tokens - 1, num_tokens - 1)
+                else:
+                    # Initialize with infinity for both weight and distance
+                    min_cut_tuple = (float('inf'), float('inf'))
+                    best_cut = window_start 
+                    # The absolute ideal cut based on target size alone 
+                    ideal_cut = chunk_start + target_size_tokens - 1
+                    
+                    for i in range(window_start, actual_window_end):
+                        distance_to_ideal = abs(i - ideal_cut)
+                        # Create a tuple: (Primary Sorting, Secondary Sorting)
+                        current_cut = (graph[i], distance_to_ideal)
+                        # If there is a tie, it will pick the one with the smaller distance_to_ideal.
+                        if current_cut < min_cut_tuple:
+                            min_cut_tuple = current_cut
+                            best_cut = i
+                    
+                    chunk_end = best_cut
+            if full_sentence_inclusion and chunk_end in token_to_sent:
+                # token_to_sent mapping looks like: {token_idx: (sentence_start_token, sentence_end_token)}
+                # We grab the end token of the sentence
+                chunk_end = token_to_sent[chunk_end][1]
+                
+                # Ensure we don't accidentally push past the total number of tokens
+                chunk_end = min(chunk_end, num_tokens - 1)
+                
+            chunks.append(text[offsets[chunk_start][0]:offsets[chunk_end][1]])
+            
+            # Advance chunk_start to the token IMMEDIATELY FOLLOWING the cut
+            # to prevent duplicating a token across two chunks.
+            chunk_start = chunk_end + 1
+
+        return chunks 
+
     
     ## IDEA 1: Treat each cluster as a monolith. This means that there is one edge per cluster, which goes from the first entity mention to the last entity mention. The weights are the maximum weight of the edges across the cluster 
     def segment_monolithic_cluster(self,data_path, target_size_tokens=384, search_window=128, test=True):
@@ -73,8 +227,10 @@ class Segmenter:
         
         k = 0 # track number of docs in text data 
         for doc in text_data:
-            full_text = (doc.get('abstract') or "") + "\n" + (doc.get('text') or "")
+            full_text = text_data# doc # (doc.get('abstract') or "") + "\n" + (doc.get('text') or "")
             if not full_text.strip(): continue
+            k = k + 1 
+            if k==1: continue
             # full_text = small_example #TEST 
             # add_special_tokens=False prevents [CLS]/[SEP] from messing with overlaps
             encoded = tokenizer(full_text, return_offsets_mapping=True, add_special_tokens=False)
@@ -160,10 +316,10 @@ class Segmenter:
 
                 chunks_text.append(tokenizer.decode(chunk_ids))
                 
-                print(f"Created chunk: {start_idx} to {best_cut_idx} (Weighted Overlap: {min_overlap})")
+                print(f"Created chunk: {start_idx} to {best_cut_idx} (Weighted Overlap: {min_overlap}, size = {best_cut_idx-start_idx})")
                 start_idx = best_cut_idx
                 
-            file_name = f"doc_{k}_segmented_monolithic.txt"
+            file_name = f"segments/doc_{k}_segmented_monolithic.txt"
             with open(file_name, "w", encoding="utf-8") as f:
                 f.write("=== ORIGINAL TEXT ===\n")
                 f.write(full_text)
@@ -172,7 +328,6 @@ class Segmenter:
             
             if(test):
                 break # early break for testing
-            k = k + 1 
 
         return chunks_text
 
@@ -185,8 +340,10 @@ class Segmenter:
 
         k = 0 
         for doc in text_data:
-            full_text = (doc.get('abstract') or "") + "\n" + (doc.get('text') or "")
+            full_text = text_data# doc # (doc.get('abstract') or "") + "\n" + (doc.get('text') or "")
             if not full_text.strip(): continue
+            k = k + 1
+            if k == 1: continue
             # full_text = small_example # test 
             
             encoded = tokenizer(full_text, return_offsets_mapping=True, add_special_tokens=False)
@@ -282,10 +439,10 @@ class Segmenter:
                 chunks_text.append(tokenizer.decode(chunk_ids))
                 
 
-                print(f"Created chunk: {start_idx} to {best_cut_idx} (Weighted Overlap {min_overlap})")
+                print(f"Created chunk: {start_idx} to {best_cut_idx} (Weighted Overlap {min_overlap} size = {best_cut_idx-start_idx})")
                 start_idx = best_cut_idx
                 
-            file_name = f"doc_{k}_segmented_most_recent.txt"
+            file_name = f"segments/doc_{k}_segmented_most_recent.txt"
             with open(file_name, "w", encoding="utf-8") as f:
                 f.write("=== ORIGINAL TEXT ===\n")
                 f.write(full_text)
@@ -294,7 +451,6 @@ class Segmenter:
             
             if(test):
                 break # early break for testing
-            k = k + 1 
 
         return chunks_text
 
@@ -307,8 +463,10 @@ class Segmenter:
 
         k = 0 
         for doc in text_data:
-            full_text = (doc.get('abstract') or "") + "\n" + (doc.get('text') or "")
+            full_text = text_data # (doc.get('abstract') or "") + "\n" + (doc.get('text') or "")
             if not full_text.strip(): continue
+            k = k + 1 
+            if k==1: continue
             # full_text = small_example
             
             #tokenize text to get input_ids and character offsets mapping
@@ -318,6 +476,7 @@ class Segmenter:
             num_tokens = len(input_ids)
 
             sent_char_spans = [m.span() for m in re.finditer(r'[^.!?\n]+[.!?\n]*', full_text)] 
+            sent_char_spans = [m.span() for m in re.finditer(r'[ghuadshfdsadsafdsafdsadwsfe]', full_text)] 
             
             token_to_sent = {}
             for s_start, s_end in sent_char_spans:
@@ -409,10 +568,10 @@ class Segmenter:
                 chunk_ids = input_ids[expanded_start:expanded_end]
                 chunks_text.append(tokenizer.decode(chunk_ids))
                 
-                print(f"Created chunk: {start_idx} to {best_cut_idx} (Weighted Overlap {min_overlap})")
+                print(f"Created chunk: {start_idx} to {best_cut_idx} (Weighted Overlap {min_overlap} size = {best_cut_idx-start_idx})")
                 start_idx = best_cut_idx
                 
-            file_name = f"doc_{k}_segmented_low_accessible_antecedent.txt"
+            file_name = f"segments/doc_{k}_segmented_low_accessible_antecedent.txt"
             with open(file_name, "w", encoding="utf-8") as f:
                 f.write("=== ORIGINAL TEXT ===\n")
                 f.write(full_text)
@@ -420,7 +579,6 @@ class Segmenter:
                 f.write("\n=======\n".join(chunks_text))
             if(test):
                 break # early break for testing
-            k = k + 1 
 
         return chunks_text
 
--- Main/main.py
+++ Main/main.py
@@ -1,6 +1,8 @@
 import sys, os
 import torch
 from transformers import AutoTokenizer
+import json
+from pathlib import Path
 
 
 CMDDROOT = os.environ['CMDDROOT'] # Expected to be a global environment variable. If not set, navigate to root of repository, call "source ./.env"
@@ -36,18 +38,84 @@ def initialize(inputArguments):
     g_ArgParse.set("device", "cuda" if torch.cuda.is_available() else "cpu")
     g_ArgParse.printArguments()
 
-
+def path_to_text(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+    return file_content 
 
 def main(inputArguments):
     initialize(inputArguments)
     
     tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
     segmenter = Segmenter(g_ArgParse.get("device"), tokenizer)
-    data_path = f"{g_ArgParse.get('dataPath')}/wikisection_en_disease_test.json"
+    
+    papers = ["1803.08375.md","1802.08129.md", "1802.03426.md", "1709.03082.md", "1612.04662.md", "2405.21046.md", "2405.21040.md", "2405.21018.md", "2405.20974.md", "2405.20774.md"]
+    papers = [f"{g_ArgParse.get('dataPath')}/LOONG/data/doc/paper/{paper}" for paper in papers]
+    
+    financials = ["2022-f10k2021_boxscorebrands.txt","2022-avni123121form10k.txt","2022-aqb-20211231x10k.txt","2021-form10-k.txt","2021-f10k2020_boxscorebrands.txt","2021-avni123120form10k.txt","2020-form10-k.txt","2020-f10k2019_boxscorebrands.txt","2020-avni123119form10k.txt","2019-avni123118form10k.txt"]
+    financials = [f"{g_ArgParse.get('dataPath')}/LOONG/data/doc/financial/{paper}" for paper in financials]
 
-    segmenter.segment_monolithic_cluster(data_path)
-    segmenter.segment_most_recent(data_path)
-    segmenter.segment_low_accessible_antecedent(data_path)
+    source_list = {"paper": papers, "financial": financials}
+    
+    method_list = ["most_recent", "most_recent_low_accessible"]
+    search_window_list = {
+        "conservative": {"target_size_tokens": 384, "search_window": 128}, 
+        "liberal": {"target_size_tokens": 258, "search_window": 254}
+    }
+    full_sentence_inclusion_list = [True, False]
+    weighted_list = [True, False]
+
+    for domain, paths in source_list.items():
+        for path in paths:
+            text = path_to_text(path)
+            source_file_name = Path(path).name 
+            
+            for method in method_list:
+                for window_name, window_params in search_window_list.items():
+                    target_size = window_params["target_size_tokens"]
+                    search_win = window_params["search_window"]
+                    
+                    for is_full_sentence in full_sentence_inclusion_list:
+                        for is_weighted in weighted_list: 
+                            
+                            print(f"Processing: {source_file_name} | {method} | {window_name} | Sent:{is_full_sentence} | Weight:{is_weighted}")
+                            
+                            # run it 
+                            chunks = segmenter.coreference_chunk_text(
+                                text=text,
+                                method=method,
+                                target_size_tokens=target_size,
+                                search_window=search_win,
+                                full_sentence_inclusion=is_full_sentence, 
+                                weighted=is_weighted
+                            )
+                            
+                            sent_folder = "full_sentence_included" if is_full_sentence else "full_sentence_excluded"
+                            weight_folder = "weighted" if is_weighted else "unweighted"
+                            
+                            # Build chunks/<paper|financial>/<source_file_name>/<method>/<full_sentence_included|excluded>/<weighted|unweighted>/<conservative|liberal>
+                            output_dir = Path("chunks") / domain / source_file_name / method / sent_folder / weight_folder / window_name
+                            output_dir.mkdir(parents=True, exist_ok=True)
+                            
+                            # json
+                            output_data = {
+                                "metadata": {
+                                    "domain": domain,
+                                    "source_file": source_file_name,
+                                    "method": method,
+                                    "sentence_inclusion": is_full_sentence,
+                                    "weighted": is_weighted,
+                                    "strategy": window_name,
+                                    "target_size_tokens": target_size,
+                                    "search_window": search_win,
+                                    "total_chunks_generated": len(chunks)
+                                },
+                                "chunks": chunks
+                            }
+                            
+                            output_file = output_dir / "chunks_output.json"
+                            with open(output_file, 'w', encoding='utf-8') as f:
+                                json.dump(output_data, f, indent=4)
 
 
 
